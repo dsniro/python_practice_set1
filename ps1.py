4ef851331514d9be587c348632566729d05f5ab6
dsniro <45084609+dsniro@users.noreply.github.com>
2019-03-26 22:48:24 -0500

    updated logic for monthly calculations
    updated each parameter within the for loop

--- ps1.py
+++ ps1.py
@@ -1,4 +1,7 @@
 #MIT6_00SCS11_ps1.pdf
+import time
+start = time.time()
+"the code you want to test stays here"
 
 # Paying Off Credit Card Debt
 
@@ -51,6 +54,53 @@ print(r_b)
     # compute the new balance.
 print("The balance for each month is as below: ")
 for i in range(1, 12):
-    print("Month " + str(i) + ":" + str(r_b))
+    print("Month " + str(i) + ":     " + str(round(r_b, 2)))
     r_b = float(r_b)
     r_b = r_b-p_p   #ok so, this isnt correct, BUT it shows how we can iterate over the range, and decrease the balance by SOME value each "month"!
+
+    mmp = mmpr*float(r_b)
+    print("the new minimum monthly payment is ")
+    print(round(mmp, 2)) #ok so I think this part is correct. 
+    
+    i_p = ((a_i_r)/(12.0))*(float(r_b))
+    print("The new interest paid is: ")
+    print(round(i_p, 2))
+
+    p_p = float(mmp) - i_p
+    print("The new Principal paid is: ")
+    print(round(p_p, 2))
+
+    #r_b = float(balance) - p_p
+    print("the remaining balance is ")
+    print(round(r_b, 2))
+
+    #print("")
+    print("")
+print("Pretty depressing!")
+print("")
+print("note to self: Still need to output total amount paid")
+print("")
+print("")
+print("")
+# I think the idea here is that I can basically take all those equations,
+# pass in the current value, calculate it, and update it
+# constants will be minimum monthly payment rate, mmpr = 0.02
+
+# the key will be to have balance be updated into a new_balance.
+# using new_balance, we can calculate the rates again, and then move down the list
+# of other operations to do.
+# lets start with the first month. we know the new balance will be 4975
+# btw, we already have this stored as r_b (remaining balance)
+
+### several minutes later ( this whole problem only took me about an hour total (minus breaks and other interruptions)
+
+# so at this point, I went back into the for loop, updated it to include each
+# value being recalculated and now it appears to be working??
+# might need to go in, manually, and see that each one is being calculated correctly.
+# also, need to round out the numbers.
+# but hey....look at that!
+end = time.time()
+print("Executed in: ")
+print(end - start)
+print( "seconds")
+print("Brought to you by Derek Niro")
